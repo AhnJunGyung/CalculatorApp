683d92f554a68da22bcd21e9216d99ec189c937b
AhnJunGyung <ekdlrkzm@naver.com>
2024-11-14 20:01:22 +0900

    Feat: "Lv1 UILavel 구현"

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configure()
     }
-
-
+    
+    private func configure() {
+        view.backgroundColor = .black
+        label.text = "12345"
+        label.textColor = .white
+        label.textAlignment = .right
+        label.font = .boldSystemFont(ofSize: 60)
+        
+        view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.top.equalToSuperview().offset(200)
+            $0.height.equalTo(100)
+        }
+        
+        
+    }
+    
+    
 }
 
